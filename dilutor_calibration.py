5e3d30a1372ee27beede3e5a4eca5db99a3e3c3b
Shannon Toole <tooles01@nyu.edu>
2026-08-13 17:53:24 -0400

    added user parameter for max olfa value (in case max was not 1000); cleaned up calculations; allows for flowmeter values as V or integer; more comments

--- dilutor_calibration.py
+++ dilutor_calibration.py
@@ -4,9 +4,7 @@ dilutor_calibration.py
 Loads calibration tables for each MFC (main olfa, air, vacuum) and fits linear model to each.
 For the desired dilution value, calculates setpoint for air and vacuum MFCs (to match main olfa MFC).
 
-Author:         Shannon Toole
-Created:        4/09/2026
-Last Modified:  4/17/2026
+ST 2026
 '''
 
 
@@ -20,25 +18,41 @@ plt.ion()   # Enable interactive mode
 current_dir = os.getcwd()
 file_directory = os.path.join(current_dir,'calibration_tables')
 
+# For plotting
+ylims_V = [.5, 5.5]
+ylims_int = [102, 1126]
+
+################################
 # File names
 olfa_file = '2026-04-17_olfa_mfc.csv'
 air_file = '2026-04-17_air_mfc.csv'
 vac_file = '2026-04-17_vac_mfc.csv'
+olfa_max = 1000
 
+# Value to dilute to
 dilute_to = 970
 dilute_to = 900
+################################
 
 
-def load_csv(full_directory):
+def load_csv(full_filepath):
     '''
-    Give it the full directory (including file name)
-    Returns 2 lists: mfc_values, flowmeter_values
+    Load MFC and flowmeter values from a CSV file.
+    Skips the first two header rows.
+
+    Parameters
+        full_filepath : str
+            Full path to the CSV file (including file name & extension)
+
+    Returns
+        mfc_values, flowmeter_values : list of float
+            Values from the first and second columns, respectively
     '''
 
     mfc_values = []
     flowmeter_values = []
     
-    with open(full_directory,newline='') as f:
+    with open(full_filepath,newline='') as f:
         csv_reader = csv.reader(f)      # Create reader object that will process lines from f (file)
         firstLine = next(csv_reader)    # Skip over header line
         secondLine = next(csv_reader)   # Skip over second line
@@ -67,39 +81,39 @@ def calculate_mfc_quadratic(poly_,olfa_FM_dil_value):
     # Solve for x (MFC value)
     solutions = np.roots(coefficients)
 
-    #print("Solutions:", solutions)
-    #print("Real solution:", solutions[0].real if np.isreal(solutions[0]) else solutions[1].real)
-
     the_solution = None
     # Since it's quadratic there will be two, so pick the one that.... is between 0 and 1000
     for solution in solutions:
-        #if (solution > 0) and (solution < 1000):
-        #    the_solution = solution
-        #    break
         if np.isreal(solution):
-            if (solution > 0):
-                if (solution < 1000):
+            if (solution > 0) and (solution < 1000):
+                if the_solution is None:
+                    print('\tfound a solution')
                     the_solution = solution
-                    break
                 else:
-                    the_solution = solution
-                    print("solution is larger than 1000")
-
+                    print('\tWARNING WARNING found 2 solutions!!!')
+            else:
+                print('\tsolution is real but not within range')
+        else:
+            print('\tsolution is not a real number')
     
     if the_solution is None:
-        print("big error")
+        print("big error: did not get any solutions")
 
     return the_solution
 
 def main():
     '''Load in the 3 csvs'''
-    full_dir_olfa = os.path.join(file_directory,olfa_file)  # Directory for olfa file
-    mfc_values,flowmeter_values = load_csv(full_dir_olfa)   # Load the mfc values and flowmeter values    
-    full_dir_vac = os.path.join(file_directory,vac_file)
-    mfc_vac,flowmeter_vac = load_csv(full_dir_vac)
-    full_dir_air = os.path.join(file_directory,air_file)
-    mfc_air,flowmeter_air = load_csv(full_dir_air)
+    file_path_olfa = os.path.join(file_directory,olfa_file)  # Directory for olfa file
+    mfc_values,flowmeter_values = load_csv(file_path_olfa)   # Load the mfc values and flowmeter values    
+    file_path_vac = os.path.join(file_directory,vac_file)
+    mfc_vac,flowmeter_vac = load_csv(file_path_vac)
+    file_path_air = os.path.join(file_directory,air_file)
+    mfc_air,flowmeter_air = load_csv(file_path_air)
     
+    '''Check if values are V or int (for plotting)'''
+    if max(flowmeter_values) > 100: ylims = ylims_int
+    else: ylims = ylims_V
+
     '''Calculate the quadratic fit for MFCs'''
     fit_olfa,poly_olfa = fit_quadratic(mfc_values,flowmeter_values)
     fit_air,poly_air = fit_quadratic(mfc_air,flowmeter_air)
@@ -120,13 +134,13 @@ def main():
     
     # Olfa
     x_olfa = np.linspace(min(mfc_values), max(mfc_values), 100)
-    ax_o1.scatter(mfc_values,flowmeter_values,color='r')#,label='Olfa')
+    ax_o1.scatter(mfc_values,flowmeter_values,color='r')
     ax_o1.set_title('Olfa MFC Calibration')
     ax_o1.plot(x_olfa, fit_olfa(x_olfa),'r',linewidth=2)
     
     # Air
     x_air = np.linspace(min(mfc_air), max(mfc_air), 100)
-    ax_a.scatter(mfc_air,flowmeter_air,color='b')#,label='Air')
+    ax_a.scatter(mfc_air,flowmeter_air,color='b')
     ax_a.set_title('Air MFC Calibration')
     ax_a.plot(x_air, fit_air(x_air),'b',linewidth=2)
 
@@ -146,14 +160,14 @@ def main():
     ax_o1.set_ylabel('Flowmeter Reading (Vdc)', fontsize=12)
     ax_a.set_ylabel('Flowmeter Reading (Vdc)', fontsize=12)
     ax_o1.set_xlim([-50, 1050])
-    ax_o1.set_ylim([.5, 5.5])
+    ax_o1.set_ylim(ylims)
     fig_oa.tight_layout()
 
     ################################################################
 
     '''Calculate vac MFC value'''
     # vac flowmeter value is olfa function at 1000-setpoint
-    vac_fm_value = fit_olfa(1000-dilute_to)
+    vac_fm_value = fit_olfa(olfa_max-dilute_to)
     vac_mfc_value = calculate_mfc_quadratic(poly_vac,vac_fm_value)
 
     '''Plot olfa and vac side by side'''
@@ -172,11 +186,11 @@ def main():
     ax_v.plot(x_vac, fit_vac(x_vac),'g',linewidth=2)
     
     # Olfa at (1000-dilution value)
-    vac_mfc_dil_value = 1000-dilute_to
+    vac_mfc_dil_value = olfa_max-dilute_to
     vac_FM_dil_value = fit_olfa(vac_mfc_dil_value)
     
     # Horizontal line at flowmeter value
-    ax_o2.axhline(y=vac_FM_dil_value,color='k',label=f'FM at [1000-{dilute_to}] SCCM  = ({round(vac_FM_dil_value,2)} V)')
+    ax_o2.axhline(y=vac_FM_dil_value,color='k',label=f'FM at [{olfa_max}-{dilute_to}] SCCM  = ({round(vac_FM_dil_value,2)} V)')
     ax_v.axhline(y=vac_FM_dil_value,color='k',label=f'{round(vac_FM_dil_value,2)} V ---> set MFC to {round(vac_mfc_value,2)} SCCM')
 
     # Vertical line at MFC setting
@@ -191,7 +205,7 @@ def main():
     ax_o2.set_ylabel('Flowmeter Reading (Vdc)', fontsize=12)
     ax_v.set_ylabel('Flowmeter Reading (Vdc)', fontsize=12)
     ax_o2.set_xlim([-50, 1050])
-    ax_o2.set_ylim([.5, 5.5])
+    ax_o2.set_ylim(ylims)
     fig_ov.tight_layout()
     
     ################################################################
